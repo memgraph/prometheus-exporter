a326057eca74972e44db8b3c427efe3d676124b3
Josip Mrden <josip.mrden@memgraph.io>
2023-05-22 10:42:37 +0200

    Add black

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ from prometheus_client import start_http_server
 
 from model import update_metrics
 
+
 def pull_metrics():
-    """ Pull Memgraph metrics """
+    """Pull Memgraph metrics"""
     res = requests.get("http://localhost:9092/")
     if res.status_code != 200:
         raise Exception("Status code is not 200!")
-    
+
     json_data = res.json()
 
     update_metrics(json_data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Start up the server to expose the metrics.
     start_http_server(8000)
     # Generate some requests.
